82fe532bbba8f5e552c3b558e7e6a2cfa6a9bfa6
github username <github email address>
2017-08-24 14:12:02 -0400

    pre postgres

--- app/controllers/hilights_controller.rb
+++ app/controllers/hilights_controller.rb
@@ -4,7 +4,7 @@ class HilightsController < ApplicationController
   # how do i send too the comment(s) associated with that highlight?
   # get random hilight with Highlight.order("Random()").first
   def index
-    @highlight = Highlight.first
+    @highlight = Highlight.order("Random()").first
     render json: @highlight.to_json(:include => :comments)
     # @highlights = Highlight.all.map{ |highlight| highlight.highlighted_text}
     # @sentences = @highlights.join(" ")
