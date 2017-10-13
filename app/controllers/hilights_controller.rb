f151b52587fea34260a85e86b1b634e0bb1939c4
github username <github email address>
2017-10-13 10:43:09 -0400

    removing comments for now

--- app/controllers/hilights_controller.rb
+++ app/controllers/hilights_controller.rb
@@ -5,7 +5,7 @@ class HilightsController < ApplicationController
   # get random hilight with Highlight.order("Random()").first
   def index
     @highlight = Highlight.order("Random()").first
-    render json: @highlight.to_json(:include => :comments)
+    # render json: @highlight.to_json(:include => :comments)
     # @highlights = Highlight.all.map{ |highlight| highlight.highlighted_text}
     # @sentences = @highlights.join(" ")
     # render json: @sentences
