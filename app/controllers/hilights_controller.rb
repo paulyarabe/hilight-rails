8c547a6d5ed84458fc4e9ec5f1f462d5de3f6987
github username <paulyarabe@post.harvard.edu>
2017-12-21 00:25:07 -0500

    set up backend to render at the root URL a JSON of book highlights grouped by the book titles

--- app/controllers/hilights_controller.rb
+++ app/controllers/hilights_controller.rb
@@ -4,11 +4,19 @@ class HilightsController < ApplicationController
   # how do i send too the comment(s) associated with that highlight?
   # get random hilight with Highlight.order("Random()").first
   def index
-    @highlight = Highlight.order("Random()").first
+    @sorted_by_book = Highlight.all.group_by(&:book_title)
+    # {"a random walk.." => [#<Highlight id:1...>, ...]}
+    # so, if you search the book title, or access the book title as a key,
+    # the value should be an array of that book's highlights.
+    # so on front end, make it so you can toggle the key to get all values
+
+    # @highlight = Highlight.order("Random()").first
     # render json: @highlight.to_json(:include => :comments)
     # @highlights = Highlight.all.map{ |highlight| highlight.highlighted_text}
     # @sentences = @highlights.join(" ")
-    render json: @highlight
+    # render json: @highlight
+
+    render json: @sorted_by_book
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root 'hilights#index'
-  get '/hilight', to: 'hilights#index', as: 'hilight'
+  root 'hilights#index'
+  # get '/hilight', to: 'hilights#index', as: 'hilight'
 end
