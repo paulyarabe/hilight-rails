7ea54dcda007ce0d8507c231bf3fb2b3e55ac847
Paul Yarabe <paulyarabe@post.harvard.edu>
2018-09-30 17:22:27 -0400

    added a couple books - the world as it is and hiroshima - and updated social animal

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170707182028) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "comment"
     t.string "commentator"
